3b68b636216ce3415e0a6c016fac19d36aa4b735
caojie <caojienan1008@foxmail.com>
2020-05-13 16:07:44 +0800

    1.更新《双向数据绑定》

--- jetpacklibrary/src/main/java/cn/shenzhencj/jetpacklibrary/architecture/databinding/bind4/UserObservable.kt
+++ jetpacklibrary/src/main/java/cn/shenzhencj/jetpacklibrary/architecture/databinding/bind4/UserObservable.kt
@@ -3,35 +3,37 @@ package cn.shenzhencj.jetpacklibrary.architecture.databinding.bind4
 import android.util.Log
 import androidx.databinding.BaseObservable
 import androidx.databinding.Bindable
-import androidx.databinding.library.baseAdapters.BR
+import cn.shenzhencj.jetpacklibrary.BR
 
 class UserObservable: BaseObservable() {
-    var userPassword2:String? = "123"
-    var userRemember:Boolean?=true
+    private var userPassword:String? = ""
+    private var userRemember:Boolean?=false
 
     @Bindable
     fun getUserPassword(): String? {
-        return this.userPassword2!!
+        return this.userPassword!!
     }
 
     fun setUserPassword(value:String){
-        this.userPassword2 = value
+        this.userPassword = value
+//        notifyPropertyChanged(androidx.databinding.library.baseAdapters.BR.userPassword)
         notifyPropertyChanged(BR.userPassword)
     }
 
     @Bindable
-    fun getRememberMe(): Boolean {
+    fun getUserRemember(): Boolean {
         return this.userRemember!!
     }
 
-    fun setRememberMe(value:Boolean){
+    fun setUserRemember(value:Boolean){
         this.userRemember = value
-        notifyPropertyChanged(BR.rememberMe)
+//        notifyPropertyChanged(androidx.databinding.library.baseAdapters.BR.userRemember)
+        notifyPropertyChanged(BR.userRemember)
     }
 
     fun onRefreshData(){
         Log.i("UserObservable", "onRefreshData()")
         this.setUserPassword("123456")
-        this.setRememberMe(!this.userRemember!!)
+        this.setUserRemember(!this.userRemember!!)
     }
 }
